6b249db7b1c76067d1aa5aca939427607c8cc8ab
truonglaibmw <laitb@antsprogrammatic.com>
2019-07-11 14:29:56 +0700

    Update code example java core

---
+++ JavaBasic/src/DataType.java
@@ -0,0 +1,17 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 5;
+		short s = 56;
+		int i = 110;
+		long l = 485l;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+
+	}
+
+}
---
+++ JavaBasic/src/Toantucoban.java
@@ -0,0 +1,10 @@
+
+public class Toantucoban {
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 6;
+		
+		int s = a/b;
+		System.out.println(s);
+	}
+}
